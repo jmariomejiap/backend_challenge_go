2ef84bb3a72d48ae2f5001531de985ea5862fac8
mario mejia <jmariomejiap@gmail.com>
2022-11-10 14:30:28 -0400

    add invert functionality

--- main.go
+++ main.go
@@ -18,6 +18,9 @@ func validatePayload(paylaod [][]string) error {
 	if len(paylaod) == 0 {
 		return errors.New("No content")
 	}
+	if len(paylaod) != len(paylaod[0]) {
+		return errors.New("Matrix is not square. Rows and columns must be equal")
+	}
 
 	return nil
     
@@ -62,6 +65,23 @@ func StringifyMatrix(records [][]string) string {
     return strings.Join(s, "")
 }
 
+// InvertMatrix returns a string representation of the ingested matrix where the the order is changed from row to columns. Used by the "/invert" endpoint
+func InvertMatrix(records [][]string) string {
+    var inverted []string
+    for i := range records {
+        for ii := range records[i] {
+            inverted = append(inverted, fmt.Sprintf("%s", records[ii][i]))			
+			
+			if ii == (len(records) - 1) {
+				inverted = append(inverted, fmt.Sprintf("%s", "\n"))
+			} else {
+				inverted = append(inverted, fmt.Sprintf("%s", ","))
+			}
+        }
+    }
+    return strings.Join(inverted, "")
+}
+
 
 
 
@@ -75,6 +95,16 @@ func main() {
 		}
 		
 		response := StringifyMatrix(records)
+		fmt.Fprint(w, response)
+	})
+	http.HandleFunc("/invert", func(w http.ResponseWriter, r *http.Request) {		
+		records, err := extractMatrix(w, r)
+		if err != nil {
+			w.Write([]byte(fmt.Sprintf("Oops, something went wrong:\n%s", err.Error())))
+			return	
+		}
+		
+		response := InvertMatrix(records)
 		fmt.Fprint(w, response)
 	})
 	http.ListenAndServe(":8080", nil)
